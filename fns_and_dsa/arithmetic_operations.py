22060094a357d29fbc95200e62930a61507971d2
Mostafa <elbayemoustapha@gmail.com>
2025-10-15 01:08:42 +0200

    i added the file arithmetic_operations and his main file .py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,21 @@
+def perform_operation(num1,num2,operation):
+    operations_available = ["add","subtract","multiply","divide"]
+
+    if not operation in operations_available:
+        return "Invalid operation"
+    else:
+
+        match operation:
+            case "add":
+                return num1 + num2
+            case "substract":
+                return num1 - num2
+            case "multiply":
+                return num1 * num2
+            case "divide":
+                if num2 == 0:
+                    return "You can't divide by zero"
+                else:
+                    return num1 / num2
+            case _:
+                return "Invalid operation"
