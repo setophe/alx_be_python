55a45e03040a920229a01e620c83a7931cfc2726
Mostafa <elbayemoustapha@gmail.com>
2025-10-16 23:21:38 +0200

    updated file bank_account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -9,10 +9,10 @@ class BankAccount:
         self.account_balance += amount
 
     def withdraw(self,amount):
-        if self.account_balance < amount:
+        if self.account_balance <= amount:
             return False
         else:
             self.account_balance -= amount
 
     def display_balance(self):
-        print(f"Current Balance: {self.account_balance}$")
+        print(f"Current Balance: ${self.account_balance}")
