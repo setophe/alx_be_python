c75c4c6b95849451c7d50db06dfb23cca27649b9
Mostafa <elbayemoustapha@gmail.com>
2025-10-19 11:17:44 +0200

    Task 0 Done

---
+++ oop/book_class.py
@@ -0,0 +1,15 @@
+class Book:
+
+    def __init__(self, title : str, author : str , year : int):
+        self._title = title
+        self._author = author
+        self._year = year
+
+    def __del__(self):
+        print(f"Deleting {self._title}")
+
+    def __str__(self):
+        return f"{self._title} by {self._author}, published in {self._year}"
+
+    def __repr__(self):
+        return f"{self._title} by {self._author}, {self._year}"
