243afdd2387a76b189e02363d50bb23f1c72d007
Mostafa <elbayemoustapha@gmail.com>
2025-10-19 12:28:25 +0200

    Task 2 done

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,31 @@
+from typing import override
+import math
+
+class Shape:
+
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise NotImplementedError
+
+
+class Rectangle(Shape):
+
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    @override
+    def area(self):
+        return self.length * self.width
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    @override
+    def area(self):
+        return self.radius * self.radius * math.pi
+
