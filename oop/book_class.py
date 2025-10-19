f79b3ae8352c0ee5c147520fa0db8b47a08c82f9
Mostafa <elbayemoustapha@gmail.com>
2025-10-19 11:34:44 +0200

    Task 0 Updated

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,15 +1,40 @@
 class Book:
+    """
+    A class to represent a Book with a title, author, and publication year,
+    implementing various Python magic methods.
+    """
 
-    def __init__(self, title : str, author : str , year : int):
-        self._title = title
-        self._author = author
-        self._year = year
+    def __init__(self, title, author, year):
+        """
+        Constructor method to initialize a Book instance.
+
+        Args:
+            title (str): The title of the book.
+            author (str): The author of the book.
+            year (int): The publication year of the book.
+        """
+        self.title = title
+        self.author = author
+        self.year = year
 
     def __del__(self):
-        print(f"Deleting {self._title}")
+        """
+        Destructor method called when the object is about to be destroyed.
+        Prints a message including the book's title.
+        """
+        print(f"Deleting {self.title}")
 
     def __str__(self):
-        return f"{self._title} by {self._author}, published in {self._year}"
+        """
+        String representation method, used by print() and str().
+        Returns a user-friendly string format.
+        """
+        return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
-        return f"Book('{self._title}', '{self._author}', {self._year})"
+        """
+        Official representation method, used by repr().
+        Returns a string that can be used to recreate the object.
+        """
+        # Note: Strings are enclosed in quotes, the integer year is not.
+        return f"Book('{self.title}', '{self.author}', {self.year})"
